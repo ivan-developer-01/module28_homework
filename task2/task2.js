29b83b7d048b6b2a8172e63131acd62f3a9bb6b7
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-11-08 21:13:43 +0300

    solve task 2

---
+++ task2/task2.js
@@ -0,0 +1,13 @@
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts', (res) => {
+	let data = "";
+
+	res.on("data", (chunk) => {
+		data += chunk;
+	});
+
+	res.on("end", () => {
+		console.log(JSON.parse(data).filter(post => post.userId === 1).length);
+	});
+});
